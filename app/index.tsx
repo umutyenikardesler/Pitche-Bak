41797620e152f1597eefc530cc9eea42a232c8ff
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2025-01-07 22:24:27 +0300

    index is updated

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import "@/global.css"
+
+export default function index() {
+  return (
+    <View>
+      <Text className='text-center bg-slate-600 color-white'> Red </Text>
+    </View>
+  )
+}
