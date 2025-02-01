adb1b2104d2ab96d77153f4b9348a6dba7993392
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2025-02-02 00:13:18 +0300

    db connect and index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,315 +1,148 @@
-import { Text, View, ScrollView, Image } from "react-native";
+import { Text, View, Image, FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-// import { RFValue } from "react-native-responsive-fontsize";
 import { Dimensions } from "react-native";
-import "@/global.css";
+import { supabase } from '@/services/supabase';
+import { useEffect, useState } from 'react';
 
 export default function Index() {
-
   const progress = 85;
   const screenWidth = Dimensions.get("window").width;
-  const fontSize = screenWidth > 430 ? 12 : screenWidth > 320 ? 11.5 : 10; 
+  const fontSize = screenWidth > 430 ? 12 : screenWidth > 320 ? 11.5 : 10;
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const { data, error } = await supabase
+        .from('match')
+        .select(`
+          id, title, time, date, prices, missing_groups, 
+          pitches (name)
+        `);
+
+      if (error) {
+        console.error('Veri çekme hatası:', error);
+      } else {
+        setMatches(data);
+      }
+    };
+
+    fetchMatches();
+  }, []);
+
+  const renderMatch = ({ item }) => {
+    const formattedDate = new Date(item.date).toLocaleDateString('tr-TR');
+    const startTime = item.time.split(':');
+    const startHour = parseInt(startTime[0], 10);
+    const startMinute = parseInt(startTime[1], 10);
+
+    const endTime = new Date();
+    endTime.setHours(startHour);
+    endTime.setMinutes(startMinute + 60);
+
+    const startFormatted = `${startHour < 10 ? '0' : ''}${startHour}:${startMinute < 10 ? '0' : ''}${startMinute}`;
+    const endFormatted = `${endTime.getHours() < 10 ? '0' : ''}${endTime.getHours()}:${endTime.getMinutes() < 10 ? '0' : ''}${endTime.getMinutes()}`;
+
+    const missingGroups = item.missing_groups ? item.missing_groups : [];
+
+    return (
+      <View className="bg-white rounded-lg mx-4 my-3 p-3 shadow-md">
+        <View className="flex-row">
+          <View className="w-1/4 justify-center py-2 px-1">
+            <Image
+              source={require('@/assets/images/ball.png')}
+              className="rounded-full mx-auto"
+              style={{
+                width: 80,
+                height: 80,
+                resizeMode: 'contain',
+              }}
+            />
+          </View>
+
+          <View className="w-1/2 p-2">
+            <Text className="text-lg font-bold pb-1">{item.title}</Text>
+            <View className="text-black text-base flex-row items-center">
+              <Ionicons name="location" size={16} color="black" />
+              <Text className="pl-1 text-black text-base">{item.pitches?.name ?? 'Bilinmiyor'}</Text>
+            </View>
+
+            <View className="text-black text-base flex-row items-center">
+              <Ionicons name="time-outline" size={16} color="black" />
+              <Text className="pl-1 text-black text-base">{startFormatted} - {endFormatted}</Text>
+            </View>
+
+            <View className="text-black text-base flex-row items-center">
+              <Ionicons name="calendar-outline" size={16} color="black" />
+              <Text className="pl-1 text-black text-base">{formattedDate}</Text>
+            </View>
+          </View>
+
+          <View className="w-1/4 py-2 px-1">
+            <View className="flex-row items-center justify-between">
+              <Text className="text-base font-semibold text-green-500">{item.prices} ₺</Text>
+              <Ionicons name="chevron-forward-outline" size={16} color="green" />
+            </View>
+          </View>
+        </View>
+
+        <View className="h-[1px] bg-gray-600 my-3" />
+
+        {/* Eksik grupları göster */}
+        <View className="flex-row flex-wrap max-w-full items-center justify-start">
+          {missingGroups.length > 0 && missingGroups.map((group, index) => (
+            <View key={index} className="flex-row items-center ml-2 border-solid border-2 border-gray-500 rounded-full p-1">
+              <View className={`rounded-full p-1 ${group === 'K' ? 'bg-red-500' : group === 'D' ? 'bg-blue-500' : group === 'O' ? 'bg-green-500' : 'bg-yellow-500'}`}>
+                <Text className="text-white font-bold text-md px-1">{group}</Text>
+              </View>
+              <Text className="ml-2 font-semibold pr-1">x 1</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+    );
+  };
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false} className="bg-slate-100 flex-1">
-
-      <View className="flex-row mt-3 px-3">
-        <Ionicons name="accessibility" size={16} color="black" className="pl-2" />
-        <Text className="font-bold "> KONDİSYONUN </Text>
-      </View>
-
-      <View className="bg-white rounded-lg mx-4 my-3 p-3 shadow-md">
-        {/* Progress Bar ve Yüzde */}
-        <View className="w-full mb-3 flex-row items-center">
-          {/* Progress Bar */}
-          <View className="flex-1 h-3 bg-gray-200 rounded-full overflow-hidden">
-            <View className="bg-blue-500 h-full" style={{ width: `${progress}%` }} />
-          </View>
-          {/* Yüzde */}
-          <Text className="pl-2 font-semibold text-base text-blue-500">{progress}%</Text>
-        </View>
-
-        {/* İkon ve Metin */}
-        <View className="flex-row items-center">
-          <Ionicons name="information-circle-outline" size={16} color="black" />
-          <Text className="text-xs text-slate-600 pl-2" style={{ fontSize }}>
-            Kondisyonun yaptığın maç sayısına göre değişiklik gösterebilir.
-          </Text>
-        </View>
-      </View>
-
-      {/* SENİ BEKLEYEN MAÇLAR */}
-      <View className="flex-row mt-3 px-3">
-        <Ionicons name="alarm-outline" size={16} color="black" className="pl-2" />
-        <Text className="font-bold "> SENİ BEKLEYEN MAÇLAR </Text>
-      </View>
-
-      <View className="bg-white rounded-lg mx-4 my-3 p-3 shadow-md">
-        {/* Üç Sütun */}
-        <View className="flex-row">
-          {/* 1. Sütun: Resim */}
-          <View className="w-1/4 justify-center py-2 px-1">
-            <Image
-              source={require('@/assets/images/ball.png')}
-              className="rounded-full mx-auto"
-              style={{
-                width: 80, // Genişliği kesin olarak belirt
-                height: 80, // Yüksekliği kesin olarak belirt
-                resizeMode: 'contain', // Resmin sıkıştırılmadan alanı doldurması
-              }}
-            />
+    <FlatList
+      data={matches}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={renderMatch}
+      contentContainerStyle={{ flexGrow: 1 }}
+      ListHeaderComponent={() => (
+        <>
+          {/* KONDİSYONUN Başlığı ve İçeriği */}
+          <View className="flex-row mt-3 px-3">
+            <Ionicons name="accessibility" size={16} color="black" className="pl-2" />
+            <Text className="font-bold "> KONDİSYONUN </Text>
           </View>
 
-          {/* 2. Sütun: Yazılar */}
-          <View className="w-1/2 p-2">
-            <Text className="text-lg font-bold pb-1"> Gidelim Joe </Text>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="location" size={16} color="black" />
-              <Text className="pl-1 text-black text-base"> Olea Halı Saha </Text>
+          <View className="bg-white rounded-lg mx-4 my-3 p-3 shadow-md">
+            {/* Progress Bar ve Yüzde */}
+            <View className="w-full mb-3 flex-row items-center">
+              {/* Progress Bar */}
+              <View className="flex-1 h-3 bg-gray-200 rounded-full overflow-hidden">
+                <View className="bg-blue-500 h-full" style={{ width: `${progress}%` }} />
+              </View>
+              {/* Yüzde */}
+              <Text className="pl-2 font-semibold text-base text-blue-500">{progress}%</Text>
             </View>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="time-outline" size={16} color="black" />
-              <Text className="pl-1 text-black text-base"> 23.00 - 24.00 </Text>
-            </View>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="calendar-outline" size={16} color="black"/>
-              <Text className="pl-1 text-black text-base"> 25.05.2025 </Text>
+
+            {/* İkon ve Metin */}
+            <View className="flex-row items-center">
+              <Ionicons name="information-circle-outline" size={16} color="black" />
+              <Text className="text-xs text-slate-600 pl-2" style={{ fontSize }}>
+                Kondisyonun yaptığın maç sayısına göre değişiklik gösterebilir.
+              </Text>
             </View>
           </View>
 
-          {/* 3. Sütun: İkonlar */}
-          <View className="w-1/4 py-2 px-1">
-            <View className="flex-row items-center justify-between">
-              <Text className="text-base font-semibold text-green-500">1.000 ₺</Text>
-              <Ionicons name="chevron-forward-outline" size={16} color="green" />
-            </View>
+          {/* SENİ BEKLEYEN MAÇLAR Başlığı */}
+          <View className="flex-row mt-3 px-3">
+            <Ionicons name="alarm-outline" size={16} color="black" className="pl-2" />
+            <Text className="font-bold "> SENİ BEKLEYEN MAÇLAR </Text>
           </View>
-        </View>
-        {/* Çizgi */}
-        <View className="h-[1px] bg-gray-600 my-3" />
-        {/* Tek Satırlı Yazı */}
-        <View className="flex-row flex-wrap max-w-full items-center justify-start ">
-
-          <View className="flex-row items-center">
-            <Text className="font-semibold">Eksikler:</Text>
-          </View>
-
-          {/* K Grubu */}
-          <View className="flex-row items-center ml-2 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-red-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">K</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 1</Text>
-          </View>
-          {/* D Grubu */}
-          <View className="flex-row items-center ml-2 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-blue-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">D</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 2</Text>
-          </View>
-          {/* O Grubu */}
-          <View className="flex-row items-center ml-2 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-green-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">O</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 3</Text>
-          </View>
-          {/* F Grubu */}
-          <View className="flex-row items-center ml-2 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-yellow-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">F</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 1</Text>
-          </View>
-
-        </View>
-        
-      </View>
-
-      {/* KADROSU EKSİK MAÇLAR */}
-      <View className="flex-row mt-3 px-3">
-        <Ionicons name="megaphone-outline" size={16} color="black" className="pl-2" />
-        <Text className="font-bold "> KADROSU EKSİK MAÇLAR  </Text>
-      </View>
-      
-      {/* Maç1 */}
-      <View className="bg-white rounded-lg mx-4 my-3 p-3 shadow-md">
-        {/* Üç Sütun */}
-        <View className="flex-row">
-          {/* 1. Sütun: Resim */}
-          <View className="w-1/4 justify-center py-2 px-1">
-            <Image
-              source={require('@/assets/images/ball.png')}
-              className="rounded-full mx-auto"
-              style={{
-                width: 80, // Genişliği kesin olarak belirt
-                height: 80, // Yüksekliği kesin olarak belirt
-                resizeMode: 'contain', // Resmin sıkıştırılmadan alanı doldurması
-              }}
-            />
-          </View>
-
-          {/* 2. Sütun: Yazılar */}
-          <View className="w-1/2 p-2">
-            <Text className="text-lg font-bold pb-1"> Hadi Maça Gidelim </Text>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="location" size={16} color="black" />
-              <Text className="pl-1 text-black text-base"> Kardeşler Halı Saha </Text>
-            </View>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="time-outline" size={16} color="black" />
-              <Text className="pl-1 text-black text-base"> 18.00 - 19.00 </Text>
-            </View>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="calendar-outline" size={16} color="black"/>
-              <Text className="pl-1 text-black text-base"> 15.04.2025 </Text>
-            </View>
-          </View>
-
-          {/* 3. Sütun: İkonlar */}
-          <View className="w-1/4 py-2 px-1">
-            <View className="flex-row items-center justify-between">
-              <Text className="text-base font-semibold text-green-500">1.250 ₺</Text>
-              <Ionicons name="chevron-forward-outline" size={16} color="green" />
-            </View>
-          </View>
-        </View>
-        {/* Çizgi */}
-        <View className="h-[1px] bg-gray-600 my-3" />
-        {/* Tek Satırlı Yazı */}
-        <View className="flex-row justify-start flex-wrap max-w-full items-center">
-
-          <View className="flex-row items-center">
-            <Text className="font-semibold">Eksikler:</Text>
-          </View>
-
-          {/* K Grubu */}
-          <View className="flex-row items-center ml-1.5 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-red-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">K</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 1</Text>
-          </View>
-          {/* D Grubu
-          <View className="flex-row items-center ml-1.5 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-blue-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">D</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 2</Text>
-          </View>
-          {/* O Grubu 
-          <View className="flex-row items-center ml-1.5 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-green-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">O</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 3</Text>
-          </View> */}
-          
-          <View className="flex-row items-center ml-1.5 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-yellow-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">F</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 1</Text>
-          </View>
-
-        </View>
-        
-      </View>
-
-       {/* Maç2 */}
-       <View className="bg-white rounded-lg mx-4 my-3 p-3 shadow-md">
-        {/* Üç Sütun */}
-        <View className="flex-row">
-          {/* 1. Sütun: Resim */}
-          <View className="w-1/4 justify-center py-2 px-1">
-            <Image
-              source={require('@/assets/images/ball.png')}
-              className="rounded-full mx-auto"
-              style={{
-                width: 80, // Genişliği kesin olarak belirt
-                height: 80, // Yüksekliği kesin olarak belirt
-                resizeMode: 'contain', // Resmin sıkıştırılmadan alanı doldurması
-              }}
-            />
-          </View>
-
-          {/* 2. Sütun: Yazılar */}
-          <View className="w-1/2 p-2">
-            <Text className="text-lg font-bold pb-1"> Hadi Maça Gidelim </Text>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="location" size={16} color="black" />
-              <Text className="pl-1 text-black text-base"> Kardeşler Halı Saha </Text>
-            </View>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="time-outline" size={16} color="black" />
-              <Text className="pl-1 text-black text-base"> 18.00 - 19.00 </Text>
-            </View>
-            
-            <View className="text-black text-base flex-row items-center">
-              <Ionicons name="calendar-outline" size={16} color="black"/>
-              <Text className="pl-1 text-black text-base"> 15.04.2025 </Text>
-            </View>
-          </View>
-
-          {/* 3. Sütun: İkonlar */}
-          <View className="w-1/4 py-2 px-1">
-            <View className="flex-row items-center justify-between">
-              <Text className="text-base font-semibold text-green-500">1.250 ₺</Text>
-              <Ionicons name="chevron-forward-outline" size={16} color="green" />
-            </View>
-          </View>
-        </View>
-        {/* Çizgi */}
-        <View className="h-[1px] bg-gray-600 my-3" />
-        {/* Tek Satırlı Yazı */}
-        <View className="flex-row justify-start flex-wrap max-w-full items-center">
-
-          <View className="flex-row items-center">
-            <Text className="font-semibold">Eksikler:</Text>
-          </View>
-
-          {/* K Grubu
-          <View className="flex-row items-center ml-1.5 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-red-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">K</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 1</Text>
-          </View> */}
-          {/* D Grubu */}
-          <View className="flex-row items-center ml-1.5 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-blue-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">D</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 2</Text>
-          </View>
-          {/* O Grubu */}
-          <View className="flex-row items-center ml-1.5 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-green-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">O</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 3</Text>
-          </View>
-          
-          {/* <View className="flex-row items-center ml-1.5 border-solid border-2 border-gray-500 rounded-full p-1">
-            <View className="bg-yellow-500 rounded-full p-1">
-              <Text className="text-white font-bold text-md px-1">F</Text>
-            </View>
-            <Text className="ml-2 font-semibold pr-1">x 1</Text>
-          </View> */}
-
-        </View>
-        
-      </View>
-
-    </ScrollView>
+        </>
+      )}
+    />
   );
 }
---
+++ services/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://fqcjmrcnvrtqcytweyce.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxY2ptcmNudnJ0cWN5dHdleWNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0MjgzNTEsImV4cCI6MjA1NDAwNDM1MX0.wikCIj1tRlJuF7NDRn913fWdq1riabtmUGUxNOezFVM';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
