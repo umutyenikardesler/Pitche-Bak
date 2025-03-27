9db744053b6b416eb029391c0fa5f0c9358150f7
Umut Yenikardeşler <umutyenikardesler@gmail.com>
2025-03-27 18:22:08 +0300

    customheader update

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, Image } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
@@ -11,8 +11,17 @@ const CustomHeader = ({ title, showNotificationIcon = true }) => {
   };
 
   return (
-    <View className="flex-row justify-between items-center px-4 py-3 bg-white">
-      <Text className="text-lg font-bold">{title}</Text>
+    <View className="flex-row justify-between items-center px-4 py-2 bg-white">
+      {/* Sayfa Başlığı (Sol Taraf) */}
+      <Text className="text-lg font-bold flex-1">{title}</Text>
+
+      {/* Orta Kısım: Logo */}
+      <Image
+        source={require("@/assets/images/logo.jpeg")} // Logonu buraya ekleyebilirsin
+        style={{ width: 180, height: 60, resizeMode: 'contain' }}
+      />
+
+      {/* Bildirim İkonu (Sağ Taraf) */}
       {showNotificationIcon && (
         <TouchableOpacity className="ml-auto" onPress={handleNotificationsPress}>
           <Ionicons name="heart-outline" size={24} color="green" />
